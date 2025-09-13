676c175c6ef7db82317b04075e19dec07a931b1e
Henry ᛯ <henry.h.george2012@gmail.com>
2025-09-12 17:32:53 -0700

    Update plugins.lua

--- .config/nvim/plugins.lua
+++ .config/nvim/plugins.lua
@@ -1,6 +1,7 @@
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'folke/tokyonight.nvim'
-	use 'ThePrimeagen/vim-be-good'
-	vim.cmd [[colorscheme tokyonight-moon]]
+    use 'wbthomason/packer.nvim'
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use 'folke/tokyonight.nvim'
+    use 'ThePrimeagen/vim-be-good'
+    vim.cmd [[colorscheme rose-pine]]
 end)

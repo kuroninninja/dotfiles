c987e02b27812443a760f013362737b6648ee809
Henry <henry.h.george2012@gmail.com>
2025-11-25 16:35:12 -0800

    Change colorscheme from rose-pine to catppuccin

--- .config/nvim/plugins.lua
+++ .config/nvim/plugins.lua
@@ -1,8 +1,9 @@
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use({ 'catppuccin/nvim', as = 'catppuccin' })
     use 'folke/tokyonight.nvim'
     use 'ThePrimeagen/vim-be-good'
-    vim.cmd [[colorscheme rose-pine]]
+    vim.cmd [[colorscheme catppuccin]]
     use("nvim-treesitter/nvim-treesitter", {run = ':TSUpdate'})
 end)
